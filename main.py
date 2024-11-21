464a8a5e4dd38bd2b6ded4d356ae48602bdb477b
Dennis C <35651709+pikeline@users.noreply.github.com>
2024-11-21 13:17:44 -0600

    Added initial sample code (#1)

---
+++ main.py
@@ -0,0 +1,6 @@
+import pepelitos_bot as pb
+
+bot = pb.discord_bot()
+
+print(bot.key)
+print(bot.sample())
---
+++ pepelitos_bot/__init__.py
@@ -0,0 +1,1 @@
+from .discord_bot import discord_bot #this allows the discord bot to be imported straight from the pepelitos_bot package
